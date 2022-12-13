56317af1ec2b0a663c3e885a4802d699455ed91d
Village <mr.village05@gmail.com>
2022-12-12 21:26:38 -0500

    Finish day 9 part two

--- day-9/src/main.rs
+++ day-9/src/main.rs
@@ -1,32 +1,74 @@
 use std::collections::HashSet;
 
-fn move_knot(head: (i32, i32), tail: &mut (i32, i32)) {
-    if head.0 - tail.0 >= 2 && head.1 == tail.1 {
-        *tail = (head.0 - 1, head.1);
-    } else if tail.0 - head.0 >= 2 && head.1 == tail.1 {
-        *tail = (head.0 + 1, head.1);
-    } else if head.1 - tail.1 >= 2 && head.0 == tail.0 {
-        *tail = (head.0, head.1 - 1);
-    } else if tail.1 - head.1 >= 2 && head.0 == tail.0 {
-        *tail = (head.0, head.1 + 1);
-    } else if (head.0 - tail.0).abs() + (head.1 - tail.1).abs() == 3 {
-        // move the distance 1 side
-        if (head.0 - tail.0).abs() == 1 {
-            *tail = (head.0, tail.1);
-        } else {
-            *tail = (tail.0, head.1);
+fn move_knot(head: (i32, i32), tail_ref: &mut (i32, i32)) {
+    let mut tail = *tail_ref;
+    // only off by one in a single directory
+    if head.0 == tail.0 {
+        if head.1 - tail.1 == 2 {
+            tail.1 = head.1 - 1;
+        } else if head.1 - tail.1 == -2 {
+            tail.1 = head.1 + 1;
         }
-        // move tail diagonally in the direction of head
-        if head.0 - tail.0 >= 2 {
-            *tail = (head.0 - 1, tail.1);
-        } else if tail.0 - head.0 >= 2 {
-            *tail = (head.0 + 1, tail.1);
-        } else if head.1 - tail.1 >= 2 {
-            *tail = (tail.0, head.1 - 1);
-        } else if tail.1 - head.1 >= 2 {
-            *tail = (tail.0, head.1 + 1);
+    } else if head.1 == tail.1 {
+        if head.0 - tail.0 == 2 {
+            tail.0 = head.0 - 1;
+        } else if head.0 - tail.0 == -2 {
+            tail.0 = head.0 + 1;
         }
+    // up two, right 1
+    } else if head.0 - tail.0 == 1 && head.1 - tail.1 == 2 {
+        tail.0 += 1;
+        tail.1 += 1;
+    // up two, left 1
+    } else if head.0 - tail.0 == -1 && head.1 - tail.1 == 2 {
+        tail.0 -= 1;
+        tail.1 += 1;
+    // down two, right 1
+    } else if head.0 - tail.0 == 1 && head.1 - tail.1 == -2 {
+        tail.0 += 1;
+        tail.1 -= 1;
+    // down two, left 1
+    } else if head.0 - tail.0 == -1 && head.1 - tail.1 == -2 {
+        tail.0 -= 1;
+        tail.1 -= 1;
+    // right 2, up 1
+    } else if head.0 - tail.0 == 2 && head.1 - tail.1 == 1 {
+        tail.0 += 1;
+        tail.1 += 1;
+    // right 2, down 1
+    } else if head.0 - tail.0 == 2 && head.1 - tail.1 == -1 {
+        tail.0 += 1;
+        tail.1 -= 1;
+    // left 2, up 1
+    } else if head.0 - tail.0 == -2 && head.1 - tail.1 == 1 {
+        tail.0 -= 1;
+        tail.1 += 1;
+    // left 2, down 1
+    } else if head.0 - tail.0 == -2 && head.1 - tail.1 == -1 {
+        tail.0 -= 1;
+        tail.1 -= 1;
+    // up 2, right 2
+    } else if head.0 - tail.0 == 2 && head.1 - tail.1 == 2 {
+        tail.0 += 1;
+        tail.1 += 1;
+    // up 2, left 2
+    } else if head.0 - tail.0 == -2 && head.1 - tail.1 == 2 {
+        tail.0 -= 1;
+        tail.1 += 1;
+    // down 2, right 2
+    } else if head.0 - tail.0 == 2 && head.1 - tail.1 == -2 {
+        tail.0 += 1;
+        tail.1 -= 1;
+    // down 2, left 2
+    } else if head.0 - tail.0 == -2 && head.1 - tail.1 == -2 {
+        tail.0 -= 1;
+        tail.1 -= 1;
     }
+    // println!(
+    //     "head: {:?}, before tail: {:?}, after tail: {:?}",
+    //     head, tail_ref, tail
+    // );
+    *tail_ref = tail;
 }
 
 fn main() {
@@ -65,6 +107,7 @@ fn main() {
     }
 
     println!("Part One: {}", visited.len());
+    // return;
 
     let mut knots = vec![];
     for _ in 0..10 {
